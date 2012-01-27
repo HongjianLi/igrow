9ed6c1baf9b7849d6618b43680ba7e52484df66c
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-01-27 10:39:42 +0800

    Renamed BSD to FreeBSD because software written for one BSD distribution is not compatible to run on another BSD distribution.

---
+++ release/Build_FreeBSD_x86.sh
@@ -0,0 +1,6 @@
+rm -f ../bin/FreeBSD/x86/igrow ../obj/FreeBSD/x86/*.o
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/thread_pool.o ../src/thread_pool.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/operation.o ../src/operation.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/ligand.o ../src/ligand.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/main.o ../src/main.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -o ../bin/FreeBSD/x86/igrow ../obj/FreeBSD/x86/thread_pool.o ../obj/FreeBSD/x86/operation.o ../obj/FreeBSD/x86/ligand.o ../obj/FreeBSD/x86/main.o -L/home/hjli/boost_1_48_0/lib/x86 -pthread -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options
---
+++ release/Build_FreeBSD_x86_64.sh
@@ -0,0 +1,6 @@
+rm -f ../bin/FreeBSD/x86_64/igrow ../obj/FreeBSD/x86_64/*.o
+clang++ -m64 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86_64/thread_pool.o ../src/thread_pool.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m64 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86_64/operation.o ../src/operation.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m64 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86_64/ligand.o ../src/ligand.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m64 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86_64/main.o ../src/main.cpp -I/home/hjli/boost_1_48_0 -c
+clang++ -m64 -static -O3 -DNDEBUG -o ../bin/FreeBSD/x86_64/igrow ../obj/FreeBSD/x86_64/thread_pool.o ../obj/FreeBSD/x86_64/operation.o ../obj/FreeBSD/x86_64/ligand.o ../obj/FreeBSD/x86_64/main.o -L/home/hjli/boost_1_48_0/lib/x86_64 -pthread -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options
