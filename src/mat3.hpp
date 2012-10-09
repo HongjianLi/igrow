7e92e964662a4b458e280cd1dbc9016ea9383199
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-10-09 17:00:41 +0800

    Avoided nan in constructing a vec3 from a vector and an angle

--- src/mat3.hpp
+++ src/mat3.hpp
@@ -44,30 +44,45 @@ namespace igrow
 		/// Constructs a rotation matrix from a normalized axis and the cosine value of an angle.
 		mat3(const vec3& a, const fl c)
 		{
-			if (a.zero()) BOOST_ASSERT(eq(c, 1) || eq(c, -1));
-			else BOOST_ASSERT(a.normalized());
-			BOOST_ASSERT(c >= -1);
-			BOOST_ASSERT(c <=  1);
-			const fl t = 1 - c;
-			const fl ta0a0 = t * a[0] * a[0];
-			const fl ta1a1 = t * a[1] * a[1];
-			const fl ta2a2 = t * a[2] * a[2];
-			const fl ta0a1 = t * a[0] * a[1];
-			const fl ta0a2 = t * a[0] * a[2];
-			const fl ta1a2 = t * a[1] * a[2];
-			const fl s = sqrt(1 - c * c); // s = sin(acos(c))
-			const fl sa0 = s * a[0];
-			const fl sa1 = s * a[1];
-			const fl sa2 = s * a[2];
-			elems[0] = ta0a0 + c;
-			elems[1] = ta0a1 - sa2;
-			elems[2] = ta0a2 + sa1;
-			elems[3] = ta0a1 + sa2;
-			elems[4] = ta1a1 + c;
-			elems[5] = ta1a2 - sa0;
-			elems[6] = ta0a2 - sa1;
-			elems[7] = ta1a2 + sa0;
-			elems[8] = ta2a2 + c;
+			if (a.zero())
+			{
+				BOOST_ASSERT(eq(c, 1) || eq(c, -1));
+				elems[0] = 1;
+				elems[1] = 0;
+				elems[2] = 0;
+				elems[3] = 0;
+				elems[4] = 1;
+				elems[5] = 0;
+				elems[6] = 0;
+				elems[7] = 0;
+				elems[8] = 1;
+			}
+			else
+			{
+				BOOST_ASSERT(a.normalized());
+				BOOST_ASSERT(c >= -1);
+				BOOST_ASSERT(c <=  1);
+				const fl t = 1 - c;
+				const fl ta0a0 = t * a[0] * a[0];
+				const fl ta1a1 = t * a[1] * a[1];
+				const fl ta2a2 = t * a[2] * a[2];
+				const fl ta0a1 = t * a[0] * a[1];
+				const fl ta0a2 = t * a[0] * a[2];
+				const fl ta1a2 = t * a[1] * a[2];
+				const fl s = sqrt(1 - c * c); // s = sin(acos(c))
+				const fl sa0 = s * a[0];
+				const fl sa1 = s * a[1];
+				const fl sa2 = s * a[2];
+				elems[0] = ta0a0 + c;
+				elems[1] = ta0a1 - sa2;
+				elems[2] = ta0a2 + sa1;
+				elems[3] = ta0a1 + sa2;
+				elems[4] = ta1a1 + c;
+				elems[5] = ta1a2 - sa0;
+				elems[6] = ta0a2 - sa1;
+				elems[7] = ta1a2 + sa0;
+				elems[8] = ta2a2 + c;
+			}
 		}
 
 		/// Returns the value at index (i, j) where j is the lowest dimension.
