56a4d5aaf917d74b2a12a4dcf28027791036799e
Jacky Lee <JackyLeeHongJian@Gmail.com>
2014-03-27 09:31:36 +0800

    Renamed the molecular mass variable ma to mm

--- src/main.cpp
+++ src/main.cpp
@@ -287,7 +287,7 @@ int main(int argc, char* argv[])
 				<< ',' << l.num_rotatable_bonds
 				<< ',' << l.num_hb_donors
 				<< ',' << l.num_hb_acceptors
-				<< ',' << l.ma
+				<< ',' << l.mm
 				<< endl;
 		}
 	}
